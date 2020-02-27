1e8987df8d16c6fa967d74513941d0e429e41351
David Hillock <dhMacPro61@Davids-Mac-Pro-114.local>
2020-02-27 16:51:26 -0500

    feat: styled the front page blog entries

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -1,30 +1,29 @@
 <?php get_header();?>
 
-<?php
-if (have_posts()):
+<?php if (have_posts()):
 
-    // The WordPress Loop: loads post content
+// The WordPress Loop: loads post content
     while (have_posts()):
         the_post();?>
 
-																																		            <section class="banner">
-																																		                <!-- Display the hero image that is stored in wordpress -->
-																																		                <?php the_post_thumbnail('large');?>
-																																		                <!-- The rouned white logo -->
-																																		                <img class="logo-main" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg;?>" alt="Inhabitents logo">
-																																		            </section>
+			<section class="banner">
+				<!-- Display the hero image that is stored in wordpress -->
+				<?php the_post_thumbnail('large');?>
+				<!-- The rouned white logo -->
+				<img class="logo-main" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg;?>" alt="Inhabitents logo">
 
-																																		        <!-- Loop ends -->
-																																		        <?php endwhile;?>
+			</section>
 
-																	        <nav class = 'main-menu'>
-																	            <?php the_posts_navigation();?>
-																	        </nav>
+			<!-- Loop ends -->
+			<?php endwhile;?>
 
-																	    <?php else: ?>
-            <p>No posts found</p>
-    <?php endif;
-?>
+						    <nav class = 'main-menu'>
+						        <?php the_posts_navigation();?>
+						    </nav>
+
+							<?php else: ?>
+        	<p>No posts found</p>
+	<?php endif;?>
 
 
 <!-- Load the terms (categories) start-->
@@ -47,7 +46,7 @@ $file_name = $term->name . '.svg';
 // echo $term->name;
 // echo "</p>";
 ?>
-            <img src='<?php echo get_template_directory_uri() . "/images/product-type-icons/$file_name" ?>'>
+            <img class="product-section-svg" src='<?php echo get_template_directory_uri() . "/images/product-type-icons/$file_name" ?>'>
                 <?php echo category_description($term); ?>
 	        <a href="<?php echo "product-type/" . $term->slug; ?>"><button type="submit" class="front-page-product-sections-btn"><?php echo strtoupper($term->name) . ' STUFF'; ?></button></a>
 
@@ -60,21 +59,30 @@ $file_name = $term->name . '.svg';
 
 <!-- Load three blog posts start-->
         <h1 class="landing-title" >INHABITENT JOURNAL</h1>
-        <?php
-$args = array(
-    'post_type' => 'post',
-    'order' => 'ASC',
-    'numberposts' => 3,
-);
-$product_posts = get_posts($args); // returns an array of posts
 
-?>
-        <?php foreach ($product_posts as $post): setup_postdata($post);?>
-																	        <?php the_title()?>
-																	        <?php the_post_thumbnail()?>
+        <section class="journal-excerpts">
+<?php
+$args = array('numberposts' => 3, 'order' => "ASC", 'orderby' => 'date');
+$postslist = get_posts($args);
+foreach ($postslist as $post): setup_postdata($post);?>
 
-																	        <?php endforeach;
-wp_reset_postdata();?>
+				<div class="landing-blog">
+					<div class = 'post-thumb'>
+						<!-- <?php the_post_thumbnail('large');?> -->
+
+					</div>
+					<?php the_date();?>
+					<br>
+					<h1 class="green-brand Novacento-bold"><?php the_title();?></h1>
+					<a href="<?php echo get_permalink(); ?>"><button class="read-entry-button">READ ENTRY</button></a>
+				</div>
+
+			<?php endforeach;?>
+</section>
+
+
+<?php wp_reset_postdata();?>
+<!-- Always good to use the above function to clear our posts -->
 
 <!-- Load three blog posts end-->
 
@@ -87,3 +95,6 @@ wp_reset_postdata();?>
 
 <!-- Load the footer  -->
 <?php get_footer();?>
+
+
+
--- themes/inhabitent/page-about.php
+++ themes/inhabitent/page-about.php
@@ -1,34 +1,34 @@
-<?php get_header(); ?>
+<?php get_header();?>
 
 <!-- <section class = "container about-page"> -->
 
 
-   
-   
-   <?php if( have_posts() ) :
 
-    //The WordPress Loop: loads post content 
-        while( have_posts() ) :
-            the_post(); ?>
-        
-        <!-- <div class="about" 
-        style="background-image: url(<?php echo get_the_post_thumbnail_url();?>)"
-    >
-    
-        <h2><?php the_title(); ?></h2>
-        </div> -->
-        <?php the_content(); ?>
-        
-        <!-- Loop ends -->
-        <?php endwhile;?>
 
-        <?php the_posts_navigation();?>
-        
+   <?php if (have_posts()):
 
-    <?php else : ?>
+    //The WordPress Loop: loads post content
+    while (have_posts()):
+        the_post();?>
+
+		        <!-- <div class="about"
+		        style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)"
+		    >
+
+		        <h2><?php the_title();?></h2>
+		        </div> -->
+		        <?php the_content();?>
+
+		        <!-- Loop ends -->
+		        <?php endwhile;?>
+
+	        <?php the_posts_navigation();?>
+
+
+	    <?php else: ?>
             <p>No posts found</p>
     <?php endif;?>
 
 <!-- </section> -->
-    
+
 <?php get_footer();?>

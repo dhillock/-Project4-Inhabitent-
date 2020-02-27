15b69cfda856830aefdb34f358544f5f4e1610d2
David Hillock <dhMacPro61@Davids-Mac-Pro-114.local>
2020-02-27 08:27:35 -0500

    feat: add Shop Stuff images to front page

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -1,8 +1,7 @@
-//
-//
+
 <?php wp_footer();?>
-// We need to keep the above, beause WP uses this to load various scripts.
-// This needs to go as the last thing before the closing body tag
+<!-- We need to keep the above, beause WP uses this to load various scripts.
+This needs to go as the last thing before the closing body tag -->
 </body>
 
 <footer>
--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -1,72 +1,77 @@
 <?php get_header(); ?>
 
-<?php if( have_posts() ) :
+<?php 
+    if( have_posts() ) :
 
-// The WordPress Loop: loads post content 
-    while( have_posts() ) :
-        the_post(); ?>
+        // The WordPress Loop: loads post content 
+        while( have_posts() ) :
+            the_post(); ?>
 
-    <section class="banner">
-		<!-- Display the hero image that is stored in wordpress -->
-        <?php the_post_thumbnail('large');?>
-        <!-- The rouned white logo -->
-        <img class="logo-main" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg;?>" alt="Inhabitents logo">
+        <section class="banner">
+            <!-- Display the hero image that is stored in wordpress -->
+            <?php the_post_thumbnail('large');?>
+            <!-- The rouned white logo -->
+            <img class="logo-main" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg;?>" alt="Inhabitents logo">
 
-    </section>
-    
-    <!-- Loop ends -->
-    <?php endwhile;?>
+        </section>
+        
+        <!-- Loop ends -->
+        <?php endwhile;?>
 
-    <nav class = 'main-menu'>
-        <?php the_posts_navigation();?>
-    </nav>
+        <nav class = 'main-menu'>
+            <?php the_posts_navigation();?>
+        </nav>
 
-	<?php else : ?>
-        	<p>No posts found</p>
-	<?php endif;?>
+        <?php else : ?>
+                <p>No posts found</p>
+        <?php endif;
+?>
 
+
+<!-- Load the terms (categories) start-->
 
 <h1 class="landing-title" >SHOP STUFF</h1>
 
-<!-- Load the terms (categories) start-->
+        <?php 
+        $terms = get_terms(array(
+            'taxonomy' => 'product-type',
+            'hide_empty' => false
+        ));
 
-<?php 
-$terms = get_terms(array(
-    'taxonomy' => 'product-type',
-    'hide_empty' => false
-));
-
-foreach($terms as $term) :
-    $file_name = $term->name . '.svg';
-    echo "<p>";
-    echo $term->name;
-    echo "</p>";?>
-    <img src='<?php echo get_template_directory_uri() . "/images/product-type-icons/$file_name"?>'>
-<?php endforeach;?>
+        foreach($terms as $term) :
+            $file_name = $term->name . '.svg';
+            echo "<p>";
+            echo $term->name;
+            echo "</p>";?>
+            <img src='<?php echo get_template_directory_uri() . "/images/product-type-icons/$file_name"?>'>
+        <?php endforeach;?>
 
 <!-- Load the terms (categories) end -->
 
-<!-- Load the terms (categories) start-->
-<h1 class="landing-title2" >Inhabitent Journal</h1>
-<?php
-   $args = array( 
-       'post_type' => 'post', 
-       'order' => 'ASC',
-       'numberposts' => 3
-    );
-   $product_posts = get_posts( $args ); // returns an array of posts
+<!-- Load three blog posts start-->
+        <h1 class="landing-title2" >INHABITENT JOURNAL</h1>
+        <?php
+        $args = array( 
+            'post_type' => 'post', 
+            'order' => 'ASC',
+            'numberposts' => 3
+            );
+        $product_posts = get_posts( $args ); // returns an array of posts
 
-?>
-<?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-   <?php the_title() ?>
-   <?php the_post_thumbnail() ?>
-  
-<?php endforeach; wp_reset_postdata(); ?>
+        ?>
+        <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+        <?php the_title() ?>
+        <?php the_post_thumbnail() ?>
+        
+        <?php endforeach; wp_reset_postdata(); ?>
+
+<!-- Load three blog posts end-->
 
 
-<!-- Load the terms (categories) start-->
+<!-- Load Latest Adventures start-->
+        <h1 class="landing-title2" >LATEST ADVENTURES</h1>
 
-
+<!-- Load Latest Adventures end-->
 
 
 <!-- Load the footer  -->
